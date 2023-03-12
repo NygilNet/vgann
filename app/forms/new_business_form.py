b8fbcd593b3ceac6e8a2287b977e840341faa1e1
Vusal Layijov <Layijovvusal@gmail.com>
2023-03-11 18:52:04 -0800

    changes from vusal first, added create business

---
+++ app/forms/new_business_form.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TextAreaField,FloatField
+from wtforms.validators import DataRequired, NumberRange,Length
+
+
+class BusinessForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(max=150)])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    features = TextAreaField('Features')
+    address = StringField('Address', validators=[
+                          DataRequired(), Length(max=150)])
+    city = StringField('City', validators=[DataRequired(), Length(max=150)])
+    state = StringField('State', validators=[DataRequired(), Length(max=2)])
+    lng = FloatField('Longitude', validators=[DataRequired()])
+    lat = FloatField('Latitude', validators=[DataRequired()])
+    categories=StringField("Categories")
