064f075483ca30327ae10c6cf8adbea60b61955c
NygilNet <nygilnet@gmail.com>
2023-03-16 14:37:21 -0500

    FEAT: user routes for update and delete

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request, redirect
 from app.models import Business, BusinessImage, User, Category, Review, db
 from ..forms.review_form import ReviewForm
 from ..forms.new_business_form import BusinessForm
+from ..forms.signup_form import SignUpForm
 from flask_login import login_required, current_user
 from sqlalchemy import func
 
@@ -43,3 +44,46 @@ def current_user_businesses(userId):
             func.avg(Review.id)).filter(Review.business_id == business["id"]).scalar()
         togo.append(business)
     return jsonify(togo)
+
+@user_routes.route('/<int:userId>', methods=['PUT'])
+@login_required
+def edit_user(userId):
+    if not current_user.id == userId:
+        return jsonify({
+            "message": "forbidden"
+        }), 403
+
+    form = SignUpForm()
+
+    if form.validate_on_submit():
+        user = User.query.get(userId)
+        user.username = form.username.data
+        user.email = form.email.data
+        user.password = form.password.data
+
+        db.session.commit()
+        return user.to_dict()
+
+
+
+@user_routes.route('/<int:userId>', methods=['DELETE'])
+@login_required
+def delete_user(userId):
+    if not current_user.id == userId:
+        return jsonify({
+            "message": "forbidden"
+        }), 403
+
+    user = User.query.get(userId)
+
+    if not user:
+        return jsonify({
+            "message": "User not found"
+        }), 404
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({
+        "message": "User successful deleted"
+    })
